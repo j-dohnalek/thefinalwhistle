9a531df2d2814181dad9b8ac911396ac2ae89b6d
Thomas Moscrip <tmoscrip1@gmail.com>
2018-03-23 01:44:22 +0000

    init

--- finalwhistle.py
+++ finalwhistle.py
@@ -5,10 +5,5 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
 if __name__ == '__main__':
     app.run()
---
+++ rendering_methods.py
@@ -0,0 +1,10 @@
+def render_homepage():
+    return None
+
+
+def render_login_form():
+    return None
+
+
+def perform_user_login():
+    return None
