967cbfbb289df3e11353d486643832a138804f9f
Thomas Moscrip <tmoscrip1@gmail.com>
2018-03-26 09:49:32 +0100

    improved init file

--- finalwhistle/__init__.py
+++ finalwhistle/__init__.py
@@ -1,13 +1,24 @@
+import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 
-# Create app server and set configuration
+# Create server and configure
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['SECRET_KEY'] = 'debug-key'
-
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(__file__), 'test.db')
+# Try to fetch secret key from OS
+try:
+    app.config['SECRET_KEY'] = os.environ['FINALWHISTLE_SECRET_KEY']
+except KeyError:
+    # If cannot find secret key, exit unless in debug mode
+    if app.debug:
+        app.config['SECRET_KEY'] = 'debug-key'
+    else:
+        print('Trying to run outside of debug mode without OS provided secret key, exiting')
+        exit(1)
+        
 # Initialise Flask extensions
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
