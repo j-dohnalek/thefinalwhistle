aa3efcf3299eff7f68e501807559dbab62cdf289
Tom Moscrip <tmoscrip1@gmail.com>
2018-04-02 10:50:37 +0100

    login functionality

--- finalwhistle/views/forms/login.py
+++ finalwhistle/views/forms/login.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import InputRequired, Length
 
 
 class LoginForm(FlaskForm):
     email = StringField('e-mail', validators=[])
     password = PasswordField('password', validators=[])
+    remember_me = BooleanField('remember me')
+    submit = SubmitField('submit')
