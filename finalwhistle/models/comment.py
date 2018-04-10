55f7fa6438415330e8232fa53fff46ca2f03d907
Thomas Moscrip <tmoscrip1@gmail.com>
2018-04-10 14:48:17 +0100

    match comment hierarchy

--- finalwhistle/models/comment.py
+++ finalwhistle/models/comment.py
@@ -1,8 +1,23 @@
 """
 Database models for comment system
 """
+from sqlalchemy import func
+
 from finalwhistle import db
 
+
 class Comment(db.Model):
+    __abstract__ = True
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    posted_by = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    posted_at = db.Column(db.DateTime, server_default=func.now())
+    edited_at = db.Column(db.DateTime, server_onupdate=func.now())
+
+
+class ArticleComment(db.Model, Comment):
+    __tablename__ = 'article_comments'
+
+
+class MatchComment(db.Model, Comment):
+    __tablename__ = 'match_comments'
