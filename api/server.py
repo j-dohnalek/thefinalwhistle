f25bb3ffd84d9178b9a2e69e7b98cecb6d378c87
Jiri Dohnalek <thinkabout3e@googlemail.com>
2018-04-11 23:59:55 +0100

    Cleanup after parsing JSON to Database

---
+++ api/server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from football import db
+import os
+
+
+app = Flask(__name__)
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(__file__), 'test.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'debug-key'
+db.init_app(app)
+
+
+import views.parse
+import views.api
