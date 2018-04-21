4e267f48102e95b83ce741dc57ab692dde5c99b3
Alvaro Lopez Hernandez <alvarolopezhernandez@gmail.com>
2018-04-21 09:56:09 +0100

    Google OAuth improvements

--- finalwhistle/static/login.js
+++ finalwhistle/static/login.js
@@ -1,9 +1,40 @@
-function onSuccess(googleUser) {
-  console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+function onSignIn(googleUser) {
+	var profile = googleUser.getBasicProfile();
+	console.log(‘ID: ’ + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	console.log(‘Name: ’ + profile.getName());
+	console.log(‘Image URL: ’ + profile.getImageUrl());
+	console.log(‘Email: ’ + profile.getEmail()); // This is null if the ‘email’ scope is not present.
+
+	var form = $('<form></form>');
+    form.attr("method", "post");
+    form.attr("action", "/login");
+
+    var parameters = {
+        'id' : profile.getId(),
+        'name' : profile.getName(),
+        'imageUrl' : profile.getImageUrl(),
+        'email' : profile.getEmail()
+    }
+
+    $.each(parameters, function(key, value) {
+        var field = $('<input></input>');
+
+        field.attr("type", "hidden");
+        field.attr("name", key);
+        field.attr("value", value);
+
+        form.append(field);
+    });
+
+    $(document.body).append(form);
+    form.submit();
+
 }
+
 function onFailure(error) {
   console.log(error);
 }
+
 function renderButton() {
   gapi.signin2.render('my-signin2', {
     'scope': 'profile email',
