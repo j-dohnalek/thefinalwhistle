8d5cfda2cad3af8d42b331aeae5aab1dfa2994db
Tom Moscrip <tmoscrip1@gmail.com>
2018-04-29 19:46:43 +0100

    account page fix

--- finalwhistle/views/forms/edit_account_info.py
+++ finalwhistle/views/forms/edit_account_info.py
@@ -23,11 +23,25 @@ def user_real_name_or_blank():
     try:
         return current_user.real_name
     except AttributeError:
-        return ''
+        print('no real name set')
+    return ''
+
+def user_fav_team_or_blank():
+    try:
+        id = current_user.supported_team_id
+        if not id is (None or ''):
+            return id
+    except AttributeError:
+        print('no fav team selected')
+        return 1
 
 
 class EditAccountInfoForm(FlaskForm):
     real_name = StringField('Real name', default=user_real_name_or_blank())
-    favourite_team = SelectField('Favourite team', choices=generate_choices_list(), coerce=int)
+    favourite_team = SelectField('Favourite team', choices=generate_choices_list(), coerce=int, default=user_fav_team_or_blank())
     submit = SubmitField(label='Save changes')
 
+class ChangePassword(FlaskForm):
+    current_pw = StringField('Current password')
+    new_pw = StringField('New password')
+    new_pw_repeat = StringField('Repeat new password')
